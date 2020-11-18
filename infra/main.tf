0086d1cfa714ff399a36bba2274a8dba4aee4b5b
scauglog <4425250+scauglog@users.noreply.github.com>
2020-11-18 10:17:01 +0000

    update hostname due to change in scaleway dns naming

--- infra/main.tf
+++ infra/main.tf
@@ -43,7 +43,7 @@ resource "scaleway_instance_server" "master" {
 
 resource "scaleway_instance_ip" "worker1_ip" {}
 resource "scaleway_instance_server" "worker1" {
-  type  = "DEV1-XL"
+  type  = "DEV1-L"
   image = "centos_7.6"
   name  = "worker1"
 
@@ -55,7 +55,7 @@ resource "scaleway_instance_server" "worker1" {
 
 resource "scaleway_instance_ip" "worker2_ip" {}
 resource "scaleway_instance_server" "worker2" {
-  type  = "DEV1-XL"
+  type  = "DEV1-L"
   image = "centos_7.6"
   name  = "worker2"
 
@@ -67,7 +67,7 @@ resource "scaleway_instance_server" "worker2" {
 
 resource "scaleway_instance_ip" "worker3_ip" {}
 resource "scaleway_instance_server" "worker3" {
-  type  = "DEV1-XL"
+  type  = "DEV1-L"
   image = "centos_7.6"
   name  = "worker3"
 
@@ -78,55 +78,11 @@ resource "scaleway_instance_server" "worker3" {
 }
 resource "scaleway_instance_ip" "worker4_ip" {}
 resource "scaleway_instance_server" "worker4" {
-  type  = "DEV1-XL"
+  type  = "DEV1-L"
   image = "centos_7.6"
   name  = "worker4"
 
   ip_id = scaleway_instance_ip.worker4_ip.id
-
-  security_group_id = scaleway_instance_security_group.sg-hadoop-public.id
-  placement_group_id = scaleway_instance_placement_group.availability_group.id
-}
-resource "scaleway_instance_ip" "worker5_ip" {}
-resource "scaleway_instance_server" "worker5" {
-  type  = "DEV1-XL"
-  image = "centos_7.6"
-  name  = "worker5"
-
-  ip_id = scaleway_instance_ip.worker5_ip.id
-
-  security_group_id = scaleway_instance_security_group.sg-hadoop-public.id
-  placement_group_id = scaleway_instance_placement_group.availability_group.id
-}
-resource "scaleway_instance_ip" "worker6_ip" {}
-resource "scaleway_instance_server" "worker6" {
-  type  = "DEV1-XL"
-  image = "centos_7.6"
-  name  = "worker6"
-
-  ip_id = scaleway_instance_ip.worker6_ip.id
-
-  security_group_id = scaleway_instance_security_group.sg-hadoop-public.id
-  placement_group_id = scaleway_instance_placement_group.availability_group.id
-}
-resource "scaleway_instance_ip" "worker7_ip" {}
-resource "scaleway_instance_server" "worker7" {
-  type  = "DEV1-XL"
-  image = "centos_7.6"
-  name  = "worker7"
-
-  ip_id = scaleway_instance_ip.worker7_ip.id
-
-  security_group_id = scaleway_instance_security_group.sg-hadoop-public.id
-  placement_group_id = scaleway_instance_placement_group.availability_group.id
-}
-resource "scaleway_instance_ip" "worker8_ip" {}
-resource "scaleway_instance_server" "worker8" {
-  type  = "DEV1-XL"
-  image = "centos_7.6"
-  name  = "worker8"
-
-  ip_id = scaleway_instance_ip.worker8_ip.id
 
   security_group_id = scaleway_instance_security_group.sg-hadoop-public.id
   placement_group_id = scaleway_instance_placement_group.availability_group.id
@@ -198,3 +154,16 @@ resource "scaleway_instance_server" "edge6" {
   security_group_id = scaleway_instance_security_group.sg-hadoop-public.id
   placement_group_id = scaleway_instance_placement_group.availability_group.id
 }
+
+resource "scaleway_instance_ip" "edge7_ip" {}
+resource "scaleway_instance_server" "edge7" {
+  type  = "DEV1-S"
+  image = "centos_7.6"
+  name  = "edge7"
+
+  ip_id = scaleway_instance_ip.edge7_ip.id
+
+  security_group_id = scaleway_instance_security_group.sg-hadoop-public.id
+  placement_group_id = scaleway_instance_placement_group.availability_group.id
+}
+
--- infra/output.tf
+++ infra/output.tf
@@ -6,65 +6,60 @@ resource "local_file" "hosts" {
 ff02::1         ip6-allnodes
 ff02::2         ip6-allrouters
 
-${scaleway_instance_server.manager.private_ip} ${trimprefix(scaleway_instance_server.manager.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.manager.id, "fr-par-1/")}
-${scaleway_instance_server.master.private_ip} ${trimprefix(scaleway_instance_server.master.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.master.id, "fr-par-1/")}
-${scaleway_instance_server.edge1.private_ip} ${trimprefix(scaleway_instance_server.edge1.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.edge1.id, "fr-par-1/")}
-${scaleway_instance_server.edge2.private_ip} ${trimprefix(scaleway_instance_server.edge2.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.edge2.id, "fr-par-1/")}
-${scaleway_instance_server.edge3.private_ip} ${trimprefix(scaleway_instance_server.edge3.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.edge3.id, "fr-par-1/")}
-${scaleway_instance_server.edge4.private_ip} ${trimprefix(scaleway_instance_server.edge4.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.edge4.id, "fr-par-1/")}
-${scaleway_instance_server.edge5.private_ip} ${trimprefix(scaleway_instance_server.edge5.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.edge5.id, "fr-par-1/")}
-${scaleway_instance_server.edge6.private_ip} ${trimprefix(scaleway_instance_server.edge6.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.edge6.id, "fr-par-1/")}
-${scaleway_instance_server.worker1.private_ip} ${trimprefix(scaleway_instance_server.worker1.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.worker1.id, "fr-par-1/")}
-${scaleway_instance_server.worker2.private_ip} ${trimprefix(scaleway_instance_server.worker2.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.worker2.id, "fr-par-1/")}
-${scaleway_instance_server.worker3.private_ip} ${trimprefix(scaleway_instance_server.worker3.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.worker3.id, "fr-par-1/")}
-${scaleway_instance_server.worker4.private_ip} ${trimprefix(scaleway_instance_server.worker4.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.worker4.id, "fr-par-1/")}
-${scaleway_instance_server.worker5.private_ip} ${trimprefix(scaleway_instance_server.worker5.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.worker5.id, "fr-par-1/")}
-${scaleway_instance_server.worker6.private_ip} ${trimprefix(scaleway_instance_server.worker6.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.worker6.id, "fr-par-1/")}
-${scaleway_instance_server.worker7.private_ip} ${trimprefix(scaleway_instance_server.worker7.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.worker7.id, "fr-par-1/")}
-${scaleway_instance_server.worker8.private_ip} ${trimprefix(scaleway_instance_server.worker8.id, "fr-par-1/")}.priv.cloud.scaleway.com ${trimprefix(scaleway_instance_server.worker8.id, "fr-par-1/")}
+${scaleway_instance_server.manager.private_ip} ${trimprefix(scaleway_instance_server.manager.id, "fr-par-1/")}.priv.instances.scw.cloud ${trimprefix(scaleway_instance_server.manager.id, "fr-par-1/")}
+${scaleway_instance_server.master.private_ip} ${trimprefix(scaleway_instance_server.master.id, "fr-par-1/")}.priv.instances.scw.cloud ${trimprefix(scaleway_instance_server.master.id, "fr-par-1/")}
+${scaleway_instance_server.edge1.private_ip} ${trimprefix(scaleway_instance_server.edge1.id, "fr-par-1/")}.priv.instances.scw.cloud ${trimprefix(scaleway_instance_server.edge1.id, "fr-par-1/")}
+${scaleway_instance_server.edge2.private_ip} ${trimprefix(scaleway_instance_server.edge2.id, "fr-par-1/")}.priv.instances.scw.cloud ${trimprefix(scaleway_instance_server.edge2.id, "fr-par-1/")}
+${scaleway_instance_server.edge3.private_ip} ${trimprefix(scaleway_instance_server.edge3.id, "fr-par-1/")}.priv.instances.scw.cloud ${trimprefix(scaleway_instance_server.edge3.id, "fr-par-1/")}
+${scaleway_instance_server.edge4.private_ip} ${trimprefix(scaleway_instance_server.edge4.id, "fr-par-1/")}.priv.instances.scw.cloud ${trimprefix(scaleway_instance_server.edge4.id, "fr-par-1/")}
+${scaleway_instance_server.edge5.private_ip} ${trimprefix(scaleway_instance_server.edge5.id, "fr-par-1/")}.priv.instances.scw.cloud ${trimprefix(scaleway_instance_server.edge5.id, "fr-par-1/")}
+${scaleway_instance_server.edge6.private_ip} ${trimprefix(scaleway_instance_server.edge6.id, "fr-par-1/")}.priv.instances.scw.cloud ${trimprefix(scaleway_instance_server.edge6.id, "fr-par-1/")}
+${scaleway_instance_server.edge7.private_ip} ${trimprefix(scaleway_instance_server.edge7.id, "fr-par-1/")}.priv.instances.scw.cloud ${trimprefix(scaleway_instance_server.edge7.id, "fr-par-1/")}
+${scaleway_instance_server.worker1.private_ip} ${trimprefix(scaleway_instance_server.worker1.id, "fr-par-1/")}.priv.instances.scw.cloud ${trimprefix(scaleway_instance_server.worker1.id, "fr-par-1/")}
+${scaleway_instance_server.worker2.private_ip} ${trimprefix(scaleway_instance_server.worker2.id, "fr-par-1/")}.priv.instances.scw.cloud ${trimprefix(scaleway_instance_server.worker2.id, "fr-par-1/")}
+${scaleway_instance_server.worker3.private_ip} ${trimprefix(scaleway_instance_server.worker3.id, "fr-par-1/")}.priv.instances.scw.cloud ${trimprefix(scaleway_instance_server.worker3.id, "fr-par-1/")}
+${scaleway_instance_server.worker4.private_ip} ${trimprefix(scaleway_instance_server.worker4.id, "fr-par-1/")}.priv.instances.scw.cloud ${trimprefix(scaleway_instance_server.worker4.id, "fr-par-1/")}
   EOF
   filename = "${path.module}/../roles/prepare/files/hosts"
 }
 resource "local_file" "ansible_hosts" {
   content     = <<EOF
 [scm_server]
-${trimprefix(scaleway_instance_server.manager.id, "fr-par-1/")}.priv.cloud.scaleway.com
+${trimprefix(scaleway_instance_server.manager.id, "fr-par-1/")}.priv.instances.scw.cloud
 
 [db_server]
-${trimprefix(scaleway_instance_server.manager.id, "fr-par-1/")}.priv.cloud.scaleway.com
+${trimprefix(scaleway_instance_server.manager.id, "fr-par-1/")}.priv.instances.scw.cloud
 
 [utility_servers:children]
 scm_server
 db_server
 
 [gateway_servers]
-${trimprefix(scaleway_instance_server.edge1.id, "fr-par-1/")}.priv.cloud.scaleway.com        host_template=HostTemplate-Gateway
-${trimprefix(scaleway_instance_server.edge2.id, "fr-par-1/")}.priv.cloud.scaleway.com        host_template=HostTemplate-Gateway
-${trimprefix(scaleway_instance_server.edge3.id, "fr-par-1/")}.priv.cloud.scaleway.com        host_template=HostTemplate-Gateway
-${trimprefix(scaleway_instance_server.edge4.id, "fr-par-1/")}.priv.cloud.scaleway.com        host_template=HostTemplate-Gateway
-${trimprefix(scaleway_instance_server.edge5.id, "fr-par-1/")}.priv.cloud.scaleway.com        host_template=HostTemplate-Gateway
-${trimprefix(scaleway_instance_server.edge6.id, "fr-par-1/")}.priv.cloud.scaleway.com        host_template=HostTemplate-Gateway
+${trimprefix(scaleway_instance_server.edge1.id, "fr-par-1/")}.priv.instances.scw.cloud        host_template=HostTemplate-Gateway
+${trimprefix(scaleway_instance_server.edge2.id, "fr-par-1/")}.priv.instances.scw.cloud        host_template=HostTemplate-Gateway
+${trimprefix(scaleway_instance_server.edge3.id, "fr-par-1/")}.priv.instances.scw.cloud        host_template=HostTemplate-Gateway
+${trimprefix(scaleway_instance_server.edge4.id, "fr-par-1/")}.priv.instances.scw.cloud        host_template=HostTemplate-Gateway
+${trimprefix(scaleway_instance_server.edge5.id, "fr-par-1/")}.priv.instances.scw.cloud        host_template=HostTemplate-Gateway
+${trimprefix(scaleway_instance_server.edge6.id, "fr-par-1/")}.priv.instances.scw.cloud        host_template=HostTemplate-Gateway
+${trimprefix(scaleway_instance_server.edge7.id, "fr-par-1/")}.priv.instances.scw.cloud        host_template=HostTemplate-Gateway
 
 [edge_servers]
-${trimprefix(scaleway_instance_server.edge1.id, "fr-par-1/")}.priv.cloud.scaleway.com        host_template=HostTemplate-Edge    role_ref_names=HDFS-HTTPFS-1
-${trimprefix(scaleway_instance_server.edge2.id, "fr-par-1/")}.priv.cloud.scaleway.com        host_template=HostTemplate-Edge    role_ref_names=HDFS-HTTPFS-1
-${trimprefix(scaleway_instance_server.edge3.id, "fr-par-1/")}.priv.cloud.scaleway.com        host_template=HostTemplate-Edge    role_ref_names=HDFS-HTTPFS-1
-${trimprefix(scaleway_instance_server.edge4.id, "fr-par-1/")}.priv.cloud.scaleway.com        host_template=HostTemplate-Edge    role_ref_names=HDFS-HTTPFS-1
-${trimprefix(scaleway_instance_server.edge5.id, "fr-par-1/")}.priv.cloud.scaleway.com        host_template=HostTemplate-Edge    role_ref_names=HDFS-HTTPFS-1
-${trimprefix(scaleway_instance_server.edge6.id, "fr-par-1/")}.priv.cloud.scaleway.com        host_template=HostTemplate-Edge    role_ref_names=HDFS-HTTPFS-1
+${trimprefix(scaleway_instance_server.edge1.id, "fr-par-1/")}.priv.instances.scw.cloud        host_template=HostTemplate-Edge    role_ref_names=HDFS-HTTPFS-1
+${trimprefix(scaleway_instance_server.edge2.id, "fr-par-1/")}.priv.instances.scw.cloud        host_template=HostTemplate-Edge    role_ref_names=HDFS-HTTPFS-1
+${trimprefix(scaleway_instance_server.edge3.id, "fr-par-1/")}.priv.instances.scw.cloud        host_template=HostTemplate-Edge    role_ref_names=HDFS-HTTPFS-1
+${trimprefix(scaleway_instance_server.edge4.id, "fr-par-1/")}.priv.instances.scw.cloud        host_template=HostTemplate-Edge    role_ref_names=HDFS-HTTPFS-1
+${trimprefix(scaleway_instance_server.edge5.id, "fr-par-1/")}.priv.instances.scw.cloud        host_template=HostTemplate-Edge    role_ref_names=HDFS-HTTPFS-1
+${trimprefix(scaleway_instance_server.edge6.id, "fr-par-1/")}.priv.instances.scw.cloud        host_template=HostTemplate-Edge    role_ref_names=HDFS-HTTPFS-1
+${trimprefix(scaleway_instance_server.edge7.id, "fr-par-1/")}.priv.instances.scw.cloud       host_template=HostTemplate-Edge    role_ref_names=HDFS-HTTPFS-1
 
 [master_servers]
-${trimprefix(scaleway_instance_server.master.id, "fr-par-1/")}.priv.cloud.scaleway.com        host_template=HostTemplate-Master1
+${trimprefix(scaleway_instance_server.master.id, "fr-par-1/")}.priv.instances.scw.cloud        host_template=HostTemplate-Master1
 
 [worker_servers]
-${trimprefix(scaleway_instance_server.worker1.id, "fr-par-1/")}.priv.cloud.scaleway.com
-${trimprefix(scaleway_instance_server.worker2.id, "fr-par-1/")}.priv.cloud.scaleway.com
-${trimprefix(scaleway_instance_server.worker3.id, "fr-par-1/")}.priv.cloud.scaleway.com
-${trimprefix(scaleway_instance_server.worker4.id, "fr-par-1/")}.priv.cloud.scaleway.com
-${trimprefix(scaleway_instance_server.worker5.id, "fr-par-1/")}.priv.cloud.scaleway.com
-${trimprefix(scaleway_instance_server.worker6.id, "fr-par-1/")}.priv.cloud.scaleway.com
-${trimprefix(scaleway_instance_server.worker7.id, "fr-par-1/")}.priv.cloud.scaleway.com
-${trimprefix(scaleway_instance_server.worker8.id, "fr-par-1/")}.priv.cloud.scaleway.com
+${trimprefix(scaleway_instance_server.worker1.id, "fr-par-1/")}.priv.instances.scw.cloud
+${trimprefix(scaleway_instance_server.worker2.id, "fr-par-1/")}.priv.instances.scw.cloud
+${trimprefix(scaleway_instance_server.worker3.id, "fr-par-1/")}.priv.instances.scw.cloud
+${trimprefix(scaleway_instance_server.worker4.id, "fr-par-1/")}.priv.instances.scw.cloud
 
 [worker_servers:vars]
 host_template=HostTemplate-Workers
